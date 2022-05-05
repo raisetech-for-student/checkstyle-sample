45f69edc892716df66e90bd4563ba98a693e1a37
小山由人 <koyama@reytech.co.jp>
2022-05-05 20:41:57 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/raisetech/checkstylesample/CheckstyleSampleApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.checkstylesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstyleSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstyleSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/checkstylesample/CheckstyleSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.checkstylesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
