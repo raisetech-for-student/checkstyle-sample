2aaf7197b0a01298ff6719064195694302a4c1ed
小山由人 <koyama@reytech.co.jp>
2022-05-05 21:18:17 +0900

    ハードタブ -> ソフトタブに修正

--- src/main/java/com/raisetech/checkstylesample/CheckstyleSampleApplication.java
+++ src/main/java/com/raisetech/checkstylesample/CheckstyleSampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckstyleSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckstyleSampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CheckstyleSampleApplication.class, args);
+  }
 
 }
