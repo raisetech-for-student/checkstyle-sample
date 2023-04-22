fb01b5a2234b1a5904b0b8f874f18e8e60ad8f7d
yoshiki-bull <dnttz118@outlook.jp>
2023-04-22 21:34:56 +0900

    CIでのCheckstyleの動作確認のための変更

---
+++ src/main/java/com/raisetech/checkstylesample/SAMPLE/sampleClass.java
@@ -0,0 +1,22 @@
+package com.raisetech.checkstylesample.SAMPLE;
+
+public class sampleClass {
+
+    private String FirstName;
+
+    private String family_name;
+
+    private int age;
+
+    public String getFirstName() {
+      return FirstName;
+    }
+
+    public String getFamily_name() {
+      return family_name;
+    }
+
+    public int getAge() {
+      return age;
+  }
+}
